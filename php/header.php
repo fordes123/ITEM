2955754c3b354e3eb97c1883ba4334b41d86424b
fordes123 <52121395+fordes123@users.noreply.github.com>
2026-02-12 14:53:00 +0800

    fix: 自有header与自定义seo参数冲突

--- php/header.php
+++ php/header.php
@@ -28,31 +28,10 @@
 
   <meta charset="<?php $this->options->charset(); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
-  <?php if ($this->is('post')): ?>
-    <meta name="author" content="<?php $this->author(); ?>" />
-    <?php
-    if ($this->hidden || !ThemeHelper::isBlank($this->password)) {
-      echo '<meta name="robots" content="noindex, nofollow">';
-    }
-    if (!ThemeHelper::isBlank($this->fields->keywords)) {
-      echo '<meta name="keywords" content="' . htmlspecialchars($this->fields->keywords) . '">';
-    }
-    $desc = $this->fields->description ?? $this->fields->text ?? null;
-    if (!ThemeHelper::isBlank($desc)) {
-      echo '<meta name="description" content="' . htmlspecialchars($desc) . '">';
-    }
-  endif; ?>
   <meta name="referrer" content="strict-origin-when-cross-origin">
   <meta name="viewport"
     content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-  <title><?php $this->archiveTitle(array(
-    'category' => _t('分类 %s 下的文章'),
-    'search' => _t('包含关键字 %s 的文章'),
-    'tag' => _t('标签 %s 下的文章'),
-    'author' => _t('%s 发布的文章')
-  ), '', ' - '); ?><?php $this->options->title(); ?><?php if ($this->is('index')): ?> -
-      <?php $this->options->description(); ?><?php endif; ?>
-  </title>
+  <title><?php $this->archiveTitle('', '', ' | '); ?><?php $this->options->title(); ?><?php if ($this->is('index')): ?> | <?php $this->options->description() ?><?php endif; ?></title>
 
   <?php if (ThemeHelper::isBlank($this->options->favicon)): ?>
     <link rel="icon" type="image/png" href="<?php $this->options->themeUrl('/assets/image/favicon-32x32.png'); ?>"
@@ -74,7 +53,24 @@
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-  <?php $this->header(); ?>
+  <?php $headerOpts = ThemeConfig::DEFAULT_HEADER_OPTS; ?>
+  <?php if ($this->is('post') || $this->is('page')): ?>
+    <meta name="author" content="<?php $this->author(); ?>" />
+  <?php
+    if ($this->hidden || !ThemeHelper::isBlank($this->password)) {
+      echo '<meta name="robots" content="noindex, nofollow">';
+    }
+    if (!ThemeHelper::isBlank($this->fields->keywords)) {
+      $headerOpts .= '&keywords=';
+      echo '<meta name="keywords" content="' . htmlspecialchars($this->fields->keywords) . '">';
+    }
+    $desc = $this->fields->description ?? $this->fields->text ?? null;
+    if (!ThemeHelper::isBlank($desc)) {
+      $headerOpts .= '&description=';
+      echo '<meta name="description" content="' . htmlspecialchars($desc) . '">';
+    }
+  endif; ?>
+  <?php $this->header($headerOpts); ?>
   <?php $this->options->customHeader(); ?>
 </head>
 
